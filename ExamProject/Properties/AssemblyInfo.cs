799c3d20ca5cd8a0a3d8711fa2ad5a5ce8796f9c
Vitalii Rymar <vitalizzzer@gmail.com>
2016-03-28 17:20:33 +0300

    The latest

--- ExamProject/Properties/AssemblyInfo.cs
+++ ExamProject/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExamProject")]
+[assembly: AssemblyTitle("ConsoleApplication7")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ExamProject")]
+[assembly: AssemblyProduct("ConsoleApplication7")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d54be7b6-c803-4b49-a967-e11246f1eaf1")]
+[assembly: Guid("2f0e091d-3614-49e0-bc8b-2bc431b66786")]
 
 // Version information for an assembly consists of the following four values:
 //
